6c4fb31c731e4a4f32729512e29a533aa4678651
Tsukasa Gotoh <go10.2ka3@ezweb.ne.jp>
2019-02-06 02:05:06 +0900

    Controllerの名前間違いを修正

--- nengine/components/Controller.hx
+++ nengine/components/Controller.hx
@@ -2,7 +2,7 @@ package nengine.components;
 import ecs.Component;
 import nengine.math.Vec2;
 
-interface AIController extends Component
+interface Controller extends Component
 {
     public function getInput(input:ControllerInput):Bool;
     public function getDirection(input:ControllerStickInput):Vec2;
